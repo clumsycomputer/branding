74913b34e5257fecb17d17f82c66c8ded2ad7744
Jared Mathews <boat@sea.local>
2023-08-27 20:51:11 -0600

    feature(StewsLogoPage): maybe logo

--- source/pages/StewsLogoPage.tsx
+++ source/pages/StewsLogoPage.tsx
@@ -4,39 +4,72 @@ import {
 } from '../components/PerspectiveGraphic'
 
 const PALETTE = {
-  darkGreen: '#1B5E20',
+  darkGreen: '#2E7D32',
+  darkOrange: '#FF8F00',
+  yellow: '#FFD600',
 }
 
 export function StewsLogoPage() {
+  const orbitalPointsA = getOrbitalPoints({
+    orbitalResolution: 90,
+    orbitalRadius: 6,
+    ditherSize: 0.3,
+    ditherColor: PALETTE.darkGreen,
+    getPointDepthAngle: (pointStamp) =>
+      (Math.PI / 2.75) * Math.sin(3 * (2 * Math.PI * pointStamp)) + Math.PI / 7,
+    getPointSliceAngle: (pointStamp) => 2 * Math.PI * pointStamp,
+    depthCosine: Math.cos,
+    depthSine: Math.sin,
+    sliceCosine: Math.cos,
+    sliceSine: Math.sin,
+  })
+  const orbitalPointsB = getOrbitalPoints({
+    orbitalResolution: 60,
+    orbitalRadius: 6,
+    ditherSize: 0.3375,
+    ditherColor: PALETTE.darkOrange,
+    getPointDepthAngle: (pointStamp) =>
+      (Math.PI / 1.5) * Math.sin(9 * (2 * Math.PI * pointStamp)) + Math.PI / 4,
+    getPointSliceAngle: (pointStamp) => 2 * Math.PI * pointStamp,
+    depthCosine: Math.cos,
+    depthSine: Math.sin,
+    sliceCosine: Math.cos,
+    sliceSine: Math.sin,
+  })
+  const orbitalPointsC = getOrbitalPoints({
+    orbitalResolution: 40,
+    orbitalRadius: 6,
+    ditherSize: 0.4,
+    ditherColor: PALETTE.yellow,
+    getPointDepthAngle: (pointStamp) =>
+      (Math.PI / 1.25) * Math.sin(81 * (2 * Math.PI * pointStamp)) +
+      Math.PI / 2,
+    getPointSliceAngle: (pointStamp) => 2 * Math.PI * pointStamp,
+    depthCosine: Math.cos,
+    depthSine: Math.sin,
+    sliceCosine: Math.cos,
+    sliceSine: Math.sin,
+  })
   return (
     <div
       style={{
         display: 'flex',
-        maxInlineSize: 512,
+        maxInlineSize: 128,
       }}
     >
       <PerspectiveGraphic
-        backgroundColor={'lightgrey'}
+        backgroundColor={'white'}
         graphicType={'circle'}
         cameraDepth={-8}
         lightDepth={100}
         perspectiveDepthFar={100}
         perspectiveDepthNear={0.1}
         perspectiveVerticalFieldOfViewAngle={(1.75 / 3) * Math.PI}
-        someWorldPoints={getOrbitalPoints({
-          orbitalResolution: 128,
-          orbitalRadius: 5,
-          ditherSize: 0.2,
-          ditherColor: PALETTE.darkGreen,
-          getPointDepthAngle: (pointStamp) =>
-            (Math.PI / 1) * Math.sin(3 * (2 * Math.PI * pointStamp)) +
-            Math.PI / 2,
-          getPointSliceAngle: (pointStamp) => 2 * Math.PI * pointStamp,
-          depthCosine: Math.cos,
-          depthSine: Math.sin,
-          sliceCosine: Math.cos,
-          sliceSine: Math.sin,
-        })}
+        someWorldPoints={[
+          ...orbitalPointsA,
+          ...orbitalPointsB,
+          ...orbitalPointsC,
+        ]}
       />
     </div>
   )
