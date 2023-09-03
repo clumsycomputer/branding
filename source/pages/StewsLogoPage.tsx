5280a2bc270a6b953862cb87c77811a05412d37d
Jared Mathews <boat@sea.local>
2023-09-02 20:53:59 -0600

    feature(StewsLogoPage): first logo

--- source/pages/StewsLogoPage.tsx
+++ source/pages/StewsLogoPage.tsx
@@ -19,25 +19,21 @@ const PALETTE = {
 }
 
 export function StewsLogoPage() {
-  const depthLoopStructureA: LoopStructure = [[0.99, LOOP_ONE, 0, 0, 0]]
+  const depthLoopStructureA: LoopStructure = [[6 / 7, LOOP_ONE, 0, 0, 0]]
   const sliceLoopStructureA: LoopStructure = [
-    [0.675, LOOP_ONE, Math.PI / 2, 0, 0],
+    [5 / 7, LOOP_ONE, Math.PI / 2, 0, 0],
   ]
   const orbitalPointsA = getOrbitalPoints({
     ditherColor: 'black',
-    orbitalResolution: 512,
-    orbitalRadius: 11,
-    ditherSize: 0.3,
-    rotationOrientationAxis: normalizedVector([1, 0, 0]),
-    rotationAngle: (Math.PI / 2) * 1.4,
-    translationVector: [0, 9, -3],
+    orbitalResolution: 512 * 4,
+    orbitalRadius: 12,
+    ditherSize: 0.35,
+    rotationOrientationAxis: normalizedVector([1, 1, 0]),
+    rotationAngle: 0,
+    translationVector: [0, 0, -6.75],
     getPointDepthAngle: (pointStamp) =>
-      (Math.PI / 24) * Math.sin(3 * 2 * Math.PI * pointStamp) + Math.PI / 2,
-    getPointSliceAngle: (pointStamp) => 1 * 2 * Math.PI * pointStamp,
-    // depthCosine: Math.cos,
-    // depthSine: Math.sin,
-    // sliceCosine: Math.cos,
-    // sliceSine: Math.sin,
+      (Math.PI / 4) * Math.sin(7 * 2 * Math.PI * pointStamp) + Math.PI / 2,
+    getPointSliceAngle: (pointStamp) => 5 * 2 * Math.PI * pointStamp,
     depthCosine: (a) =>
       loopCosine(loopPoint(depthLoopStructureA, normalizedAngle(a))),
     depthSine: (a) =>
@@ -46,62 +42,6 @@ export function StewsLogoPage() {
       loopCosine(loopPoint(sliceLoopStructureA, normalizedAngle(a))),
     sliceSine: (a) =>
       loopSine(loopPoint(sliceLoopStructureA, normalizedAngle(a))),
-  })
-  const depthLoopStructureB: LoopStructure = [[0.99, LOOP_ONE, 0, 0, 0]]
-  const sliceLoopStructureB: LoopStructure = [
-    [0.675, LOOP_ONE, Math.PI / 2, 0, 0],
-  ]
-  const orbitalPointsB = getOrbitalPoints({
-    ditherColor: 'black',
-    orbitalResolution: 512,
-    orbitalRadius: 9,
-    ditherSize: 0.3,
-    rotationOrientationAxis: normalizedVector([1, 0, 0]),
-    rotationAngle: (Math.PI / 2) * 0.75,
-    translationVector: [0, 0, -3],
-    getPointDepthAngle: (pointStamp) =>
-      (Math.PI / 24) * Math.sin(3 * 2 * Math.PI * pointStamp) + Math.PI / 2,
-    getPointSliceAngle: (pointStamp) => 1 * 2 * Math.PI * pointStamp,
-    // depthCosine: Math.cos,
-    // depthSine: Math.sin,
-    // sliceCosine: Math.cos,
-    // sliceSine: Math.sin,
-    depthCosine: (a) =>
-      loopCosine(loopPoint(depthLoopStructureB, normalizedAngle(a))),
-    depthSine: (a) =>
-      loopSine(loopPoint(depthLoopStructureB, normalizedAngle(a))),
-    sliceCosine: (a) =>
-      loopCosine(loopPoint(sliceLoopStructureB, normalizedAngle(a))),
-    sliceSine: (a) =>
-      loopSine(loopPoint(sliceLoopStructureB, normalizedAngle(a))),
-  })
-  const depthLoopStructureC: LoopStructure = [[0.99, LOOP_ONE, 0, 0, 0]]
-  const sliceLoopStructureC: LoopStructure = [
-    [0.675, LOOP_ONE, Math.PI / 2, 0, 0],
-  ]
-  const orbitalPointsC = getOrbitalPoints({
-    ditherColor: 'black',
-    orbitalResolution: 512,
-    orbitalRadius: 10,
-    ditherSize: 0.5,
-    rotationOrientationAxis: normalizedVector([1, 0, 0]),
-    rotationAngle: (Math.PI / 2) * 0,
-    translationVector: [0, -14, -10],
-    getPointDepthAngle: (pointStamp) =>
-      (Math.PI / 24) * Math.sin(1 * 2 * Math.PI * pointStamp) + Math.PI / 2,
-    getPointSliceAngle: (pointStamp) => 1 * 2 * Math.PI * pointStamp,
-    // depthCosine: Math.cos,
-    // depthSine: Math.sin,
-    // sliceCosine: Math.cos,
-    // sliceSine: Math.sin,
-    depthCosine: (a) =>
-      loopCosine(loopPoint(depthLoopStructureC, normalizedAngle(a))),
-    depthSine: (a) =>
-      loopSine(loopPoint(depthLoopStructureC, normalizedAngle(a))),
-    sliceCosine: (a) =>
-      loopCosine(loopPoint(sliceLoopStructureC, normalizedAngle(a))),
-    sliceSine: (a) =>
-      loopSine(loopPoint(sliceLoopStructureC, normalizedAngle(a))),
   })
   return (
     <div
@@ -118,11 +58,7 @@ export function StewsLogoPage() {
         perspectiveDepthFar={100}
         perspectiveDepthNear={0.01}
         perspectiveVerticalFieldOfViewAngle={(1.75 / 3) * Math.PI}
-        someWorldPoints={[
-          ...orbitalPointsA,
-          ...orbitalPointsB,
-          ...orbitalPointsC,
-        ]}
+        someWorldPoints={[...orbitalPointsA]}
       />
     </div>
   )
